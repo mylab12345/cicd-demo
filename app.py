005eaf4888705063966bbfd113ffc0ccd28a67b0
test <test@gmail.com>
2025-11-28 21:06:06 +0530

    new files added

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
+@app.route('/healthz')
+def healthz():
+    return jsonify(status="ok"), 200
+
 @app.route('/')
-def hello():
-    return "Hello, CI/CD World!  Welcome to Devops Journey"
-              
+def home():
+    return "Hello, CI/CD World!" Welcome to DevOps...., 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
